027a7609e455f32476f42cb89522430eba8c292b
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2020-07-26 17:21:46 -0500

    tint just button

--- mySketch.js
+++ mySketch.js
@@ -208,7 +208,6 @@ if(width<1920/3&&state==STATES.QUIZ){//only do FFT for larger screens
 	if (state == STATES.STARTSCREEN) {
 
 		image(titleImg, 0, 0, width, height);
-		image(titleButton, 0, 0, width, height);
 
 
 		//drawSoundsAsWords();
@@ -218,11 +217,15 @@ if(width<1920/3&&state==STATES.QUIZ){//only do FFT for larger screens
 		//if (mouseX > width / 2 - 40 && mouseY > height / 2 - 40 - 25 && mouseY < height / 2 + 40 + 25) { 
 		if (mouseX < width * .8 && mouseX > width * .2 && mouseY > height / 2 - 100 && mouseY < height / 2 + 300) { //make it bigger since we just want people to click
 			fill(155, 150);
-			tint(200, 255, 0); //
+			//tint(200, 255, 0); //
 
 		} else {
-			fill(255, 150);
+			noTint();
+			//fill(255, 150);
 		}
+
+		image(titleButton, 0, 0, width, height);
+		noTint();
 
 	
 			textSize(50*width/1920);
