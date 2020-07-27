8516ed3afa83a7d673352dbcdbf6794c526b4469
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2020-07-27 18:26:50 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -232,10 +232,10 @@ if(width<1920/3&&state==STATES.QUIZ){//only do FFT for larger screens
 		//if (mouseX > width / 2 - 40 && mouseY > height / 2 - 40 - 25 && mouseY < height / 2 + 40 + 25) { 
 		if (mouseX < width * .8 && mouseX > width * .2 && mouseY > height / 2 - 100 && mouseY < height / 2 + 300) { //make it bigger since we just want people to click
 			//fill(155, 150);
-			startButt.tint(200, 255, 0); //
+			//startButt.tint(200, 255, 0); //
 
 		} else {
-			startButt.noTint();
+		//	startButt.noTint();
 			//fill(255, 150);
 		}
 
